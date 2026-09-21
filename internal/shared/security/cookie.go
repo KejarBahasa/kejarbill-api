9fd7e0cb9f7a10070f9cfe8f4dac5a6f7c13e6af
mramdhani <mramdhani010@gmail.com>
2026-09-21 09:18:27 +0700

    fix: update cookie path in SetRefreshCookie function to match API versioning

--- internal/shared/security/cookie.go
+++ internal/shared/security/cookie.go
@@ -13,7 +13,7 @@ func SetRefreshCookie(c fiber.Ctx, refreshToken string) {
 		HTTPOnly: true,
 		Secure:   true,
 		SameSite: "Lax",
-		Path:     "/api/auth",
+		Path:     "/v1/auth",
 		Expires: time.Now().Add(
 			30 * 24 * time.Hour,
 		),
